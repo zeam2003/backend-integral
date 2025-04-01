d613d2ee7ac7369247e9a484909a839202829178
Zeam <zeam2003@outlook.com>
2025-04-01 17:50:21 -0300

    Se agrega webapp en Flutter de prueba

--- src/main.ts
+++ src/main.ts
@@ -2,20 +2,27 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as dotenv from 'dotenv';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 dotenv.config();
 
-
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  //Configurar Cors
+  // Serve Flutter web build
+  app.useStaticAssets(join(__dirname, '..', 'public', 'flutter_web'));
+
+  // CORS configuration (already supports Flutter web requests)
   app.enableCors({
     origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
+
+  // API routes configuration
   app.setGlobalPrefix('api/v1');
+  
   const config = new DocumentBuilder()
     .setTitle('Integral API')
     .setDescription('API para la aplicación Integral')
@@ -27,7 +34,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/v1/docs', app, document);
   const port = process.env.PORT || 3001;
-  await app.listen(port, '127.0.0.1');
+  await app.listen(port, '0.0.0.0');
   console.log(`La aplicación esta corriendo en el puerto: ${port}`);
 }
 bootstrap();
